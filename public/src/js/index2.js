f31728bb53b88a5fb430569cf3d1affc8c46b206
Matheus Assola Fernandes de Oliveira <massola@sabesp.com.br>
2025-11-24 14:55:31 -0300

    efeitos especiais

--- public/src/js/index2.js
+++ public/src/js/index2.js
@@ -1,4 +1,16 @@
-
-window.addEventListener("load", () => {
-  document.body.classList.add("blackhole-in");
-});
+document.addEventListener("DOMContentLoaded", function()
+  {
+      const btnCadastro = document.getElementById("btnCadastro");
+      if(btnCadastro){
+        btnCadastro.addEventListener("click", function(){
+          window.location.href = "cadastro.html";
+        });
+      }
+      const btnLogin = document.getElementById("btnLogin");
+      if(btnLogin){
+         btnLogin.addEventListener("click", function(){
+          window.location.href = "login.html";
+         });
+      }
+  });
+        
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -3,13 +3,19 @@ document.addEventListener("DOMContentLoaded", function()
       const btnCadastro = document.getElementById("btnCadastro");
       if(btnCadastro){
         btnCadastro.addEventListener("click", function(){
-          window.location.href = "cadastro.html";
+            document.body.classList.add("blackhole-out");
+            setTimeout(() => {
+              window.location.href = "index2.html";
+            }, 1000); // espera animação terminar
         });
       }
       const btnLogin = document.getElementById("btnLogin");
       if(btnLogin){
          btnLogin.addEventListener("click", function(){
-          window.location.href = "login.html";
+            document.body.classList.add("blackhole-out");
+            setTimeout(() => {
+              window.location.href = "index2.html";
+            }, 1000); // espera animação terminar
          });
       }
   });
